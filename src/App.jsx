c44c7b27ab7455a3ab80e8245bd845043d7606d9
Github-Archives <drewreynerwilliams@gmail.com>
2023-10-27 01:05:40 -0400

    fix tailwind and add basic keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="piano">
+        <div className="octave">
+          <div className="key white" data-note="C"></div>
+          <div className="key black" data-note="C#"></div>
+          <div className="key white" data-note="D"></div>
+          <div className="key black" data-note="D#"></div>
+          <div className="key white" data-note="E"></div>
+          <div className="key white" data-note="F"></div>
+          <div className="key black" data-note="F#"></div>
+          <div className="key white" data-note="G"></div>
+          <div className="key black" data-note="G#"></div>
+          <div className="key white" data-note="A"></div>
+          <div className="key black" data-note="A#"></div>
+          <div className="key white" data-note="B"></div>
+        </div>
+        {/* <!-- add more octaves as needed --> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+
+      <h1 className="bg-sky-500 text-3xl font-bold underline hover:bg-sky-700">
+        Welcome to my website!
+      </h1>
+      <p
+        id="my-paragraph"
+        className="bg-sky-500 text-3xl font-bold underline hover:bg-sky-700"
+      >
+        Click me to change my color!
       </p>
     </>
   )
