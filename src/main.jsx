8e63ac9e74f66623d88633993c69a2090f453111
Github-Archives <drewreynerwilliams@gmail.com>
2023-11-11 20:59:11 -0500

    add comment reminder about StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // Decide if you want to remove StrictMode to stop useEffect from running twice
   <React.StrictMode>
     <App />
   </React.StrictMode>,
